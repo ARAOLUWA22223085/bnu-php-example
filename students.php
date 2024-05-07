1c7ad663a323bed54e9a15cd6104e44bb9a6079d
ARAOLUWA22223085 <114756077+ARAOLUWA22223085@users.noreply.github.com>
2024-05-07 14:52:35 +0100

    mods...

--- students.php
+++ students.php
@@ -41,6 +41,7 @@
             $data['content'] .= "<td> {$row["county"]} </td>";
             $data['content'] .= "<td> {$row["country"]} </td>";
             $data['content'] .= "<td> {$row["postcode"]} </td>";
+//            $data['content'] .= "<td> {$row["profile_picture"]} </td>";
             $data['content'] .= "<td> <input type='checkbox' name='students[]' value='$row[studentid]' ></td>";
             $data['content'] .= "</tr>";
         }
