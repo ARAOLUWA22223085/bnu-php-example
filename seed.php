38ed10496809b75cf3cd99b94245338ed8420091
ARAOLUWA22223085 <114756077+ARAOLUWA22223085@users.noreply.github.com>
2024-05-07 14:18:52 +0100

    added faker and modified seed page

--- seed.php
+++ seed.php
@@ -1,23 +1,40 @@
 <?php
 
-    include("_includes/config.inc");
-    include("_includes/dbconnect.inc");
-    include("_includes/functions.inc");
+include("_includes/config.inc");
+include("_includes/dbconnect.inc");
+include("_includes/functions.inc");
+require_once 'vendor/autoload.php';
 
+use Faker\Factory;
 
-    // Checks whether we are logged in
-    if (isset($_SESSION['id'])) {
+// Checks whether we're logged in
+if (isset($_SESSION['id'])) {
+    // Initialize Faker
+    $faker = Factory::create();
 
-        $sql = "INSERT INTO student (studentid, password, dob, firstname, 
-                lastname, house, town, county, country, postcode)
-                VALUES (value1, value2, value3, value4, value5, value6, 
-                value7, value8, value9, value10)";
+    // Prepare SQL insert statement
+    $sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        $result = mysqli_query($conn,$sql);
+    for ($i = 0; $i < 5; $i++) {
+        $student = [
+            $faker->unique()->numberBetween(20000001, 29999999),
+            $faker->password,
+            $faker->date($format = 'Y-m-d', $max = '2003-12-31'),
+            $faker->firstName,
+            $faker->lastName,
+            $faker->streetAddress,
+            $faker->city,
+            $faker->state,
+            $faker->country,
+            $faker->postcode
+        ];
 
-        // INSERT INTO table_name (column1, column2, column3, ...)
-        // VALUES (value1, value2, value3, ...);
+        // Assuming $conn is your database connection variable from dbconnect.inc
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($student);
+    }
 
-    } 
+    echo "Students inserted successfully.";
+}
 
-?> 
+?>
