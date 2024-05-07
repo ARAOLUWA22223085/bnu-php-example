27763959f7977620cccb22444bb9a30ad1083ada
ARAOLUWA22223085 <114756077+ARAOLUWA22223085@users.noreply.github.com>
2024-05-07 14:21:59 +0100

    minor phrasing edit

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -2,6 +2,6 @@
    <a href="modules.php" class="text-light">My Modules</a> |
    <a href="assignmodule.php" class="text-light">Assign Module</a> |
    <a href="details.php" class="text-light">My Details</a> |
-   <a href="students.php" class="text-light">Students</a> |
+   <a href="students.php" class="text-light">All Students</a> |
    <a href="logout.php" class="text-light">Logout</a>
 </div>
